90c6edc50ae45155e9906e6be64b0c10daef6827
lamhung2k5 <lamh31387@gmail.com>
2026-08-10 23:52:31 +0700

    Day 8: interface (finish)

--- week1/CSharpDay8/PartTimeEmployee.cs
+++ week1/CSharpDay8/PartTimeEmployee.cs
@@ -6,6 +6,15 @@ public class PartTimeEmployee : Employee
     public PartTimeEmployee(string id, string fullName, decimal baseSalary, decimal workingHours, decimal hourlyRate) : base(id, fullName, baseSalary)
     {
     //khong biet co can kiem tra dieu kien khong, minh bo qua
+        if(workingHours <= 0)
+        {
+            throw new ArgumentException("so gio lam khong nho hon bang 0", nameof(workingHours));
+        }
+
+        if(hourlyRate <= 0)
+        {
+            throw new ArgumentException("luong  khong nho hon bang 0", nameof(hourlyRate));
+        }
         WorkingHours = workingHours;
         HourlyRate = hourlyRate;
     }
@@ -15,6 +24,15 @@ public class PartTimeEmployee : Employee
         return WorkingHours * HourlyRate;
     }
 
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Id: {Id}");
+        Console.WriteLine($"Full Name: {FullName}");
+        Console.WriteLine($"Working Hours: {WorkingHours}");
+        Console.WriteLine($"Hourly Rate: {HourlyRate:N0} đồng");
+        Console.WriteLine(
+            $"Total Salary: {CalculateSalary():N0} đồng");
+    }
 
 
 }
